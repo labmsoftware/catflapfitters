c4aa9c98b8429a4e9ca79c50a239cd2b14496829
Benjamin Moss <benmoss2002@fastmail.co.uk>
2024-03-16 16:15:16 +0000

    add products page

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,7 @@
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 use App\Http\Action\View\ViewHomeAction;
+use App\Http\Action\View\ViewProductsAction;
 
 // TODO: Add CORS middleware to protect routes
 
@@ -10,5 +11,6 @@ return function(App $app) {
 
     $app->group('', function(RouteCollectorProxy $views) {
         $views->get('/', ViewHomeAction::class);
+        $views->get('/products', ViewProductsAction::class);
     });
 };
